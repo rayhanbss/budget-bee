04ffadd04925a767f104d592d594dcc14fa250cd
Rayhan <masbaguss001@gmail.com>
2025-05-22 22:04:05 +0700

    create the necessary files and add the colors

--- app/src/main/java/com/example/budgetbee/ui/theme/Theme.kt
+++ app/src/main/java/com/example/budgetbee/ui/theme/Theme.kt
@@ -12,25 +12,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = YellowPrimary,
+    secondary = YellowSecondary,
+    tertiary = YellowTertiary,
+
+    background = White,
+    surface = White,
+    onPrimary = White,
+    onSecondary = Grey,
+    onTertiary = Black,
+    onBackground = Black,
+    onSurface = Black,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = YellowPrimary,
+    secondary = YellowSecondary,
+    tertiary = YellowTertiary,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = White,
+    surface = White,
+    onPrimary = White,
+    onSecondary = Grey,
+    onTertiary = Black,
+    onBackground = Black,
+    onSurface = Black,
 )
 
 @Composable
